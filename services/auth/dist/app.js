00466e9868d1fddf9eeab6d492f78721ec5a130e
krishabodar-30 <krishabodar07@gmail.com>
2026-05-06 17:40:31 +0530

    first commit

---
+++ services/auth/dist/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import { connectKafka } from "./producer.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectKafka();
+app.use("/api/auth", authRoutes);
+export default app;
